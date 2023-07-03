67f0c82ad80e2f89a8d633ef39c1a04d5b28cd96
photogrammer <khsj15@khu.ac.kr>
2023-07-04 00:06:24 +0900

    chore: replace printf to ft_printf

--- sources/mandatory/show_alloc_mem.c
+++ sources/mandatory/show_alloc_mem.c
@@ -1,6 +1,6 @@
+#include "libft.h"
 #include "malloc.h"
 #include "malloc_util.h"
-#include "libft.h"
 
 int __show_alloc_mem(t_page_header *page) {
   t_metadata *block;
@@ -10,7 +10,7 @@ int __show_alloc_mem(t_page_header *page) {
 
   while (page) {
     ft_printf("page: %p - %p size : %u bytes\n", page->first_block,
-           (char *)page->first_block + page->size, page->size);
+              (char *)page->first_block + page->size, page->size);
     block = page->first_block;
     page_block_sum = 0;
     while (page_block_sum < page->size) {
@@ -20,7 +20,7 @@ int __show_alloc_mem(t_page_header *page) {
         break;
       if (IS_ALLOCATED(block->header)) {
         ft_printf("%p - %p : %u bytes (allocated)\n", block,
-               (char *)block + block_size, block_size);
+                  (char *)block + block_size, block_size);
         total += block_size;
       }
       block = (t_metadata *)((char *)block + block_size);
@@ -34,18 +34,18 @@ void show_alloc_mem(void) {
   t_arena *arena = &g_arena;
   size_t total = 0;
 
-  printf("allocated memory blocks:\n");
+  ft_printf("allocated memory blocks:\n");
   if (arena->tiny_page) {
-    printf("TINY\n");
+    ft_printf("TINY\n");
     total += __show_alloc_mem(arena->tiny_page);
   }
   if (arena->small_page) {
-    printf("SMALL\n");
+    ft_printf("SMALL\n");
     total += __show_alloc_mem(arena->small_page);
   }
   if (arena->large_page) {
-    printf("LARGE\n");
+    ft_printf("LARGE\n");
     total += __show_alloc_mem(arena->large_page);
   }
-  printf("Total : %zu bytes\n", total);
+  ft_printf("Total : %u bytes\n", total);
 }
