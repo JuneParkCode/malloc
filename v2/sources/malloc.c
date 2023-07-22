e1e7a3ebc36ab6e5b75c3ddd46826c3fe329848e
Photogrammer <khsj15@khu.ac.kr>
2023-07-22 17:27:24 +0900

    create v2

---
+++ v2/sources/malloc.c
@@ -0,0 +1,13 @@
+#include "malloc.h"
+#include "malloc_util.h"
+#include <stdio.h>
+
+void *malloc(unsigned long size) __attribute__((visibility("default")));
+
+/**
+ * @fn malloc
+ * @brief allocate memory block by size
+ * @param size size
+ * @return a pointer to the newly allocated memory, or NULL if failed
+ */
+void *malloc(unsigned long size) { return ((void *)(size)); }
---
+++ v2/sources/malloc_util.h
@@ -0,0 +1,4 @@
+#ifndef MALLOC_UTIL_H
+#define MALLOC_UTIL_H
+
+#endif
